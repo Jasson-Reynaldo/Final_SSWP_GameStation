535fe0cdeab10a5c0f0f95f648a77424db1a9861
Francis-Dwiputra-A <58261826+Francis-Dwiputra-A@users.noreply.github.com>
2020-12-11 17:21:15 +0700

    improvement for game page and controller

--- GameStation/GameList/controllers/game_controller.py
+++ GameStation/GameList/controllers/game_controller.py
@@ -6,22 +6,35 @@ def index(request):
     if request.method == 'POST':
         search = request.POST.dict()
         name = search['name']
-        choice = search['choice']
+        choice_name = search['choice_name']
+        choice_price = search['choice_price']
         price = search['price']
         games = Game.objects.all()
-        if name:
-            games = games.filter(title__icontains=name)
-        if choice and price:
-            if choice == 'equal':
+        if choice_name and name:
+            if choice_name == 'title':
+                games = games.filter(title__icontains=name)
+            elif choice_name == 'genre':
+                games = games.filter(genre__name__icontains=name)
+            elif choice_name == 'developer':
+                games = games.filter(developer__last_name__icontains=name) | games.filter(developer__first_name__icontains=name)
+            elif choice_name == 'publisher':
+                games = games.filter(publisher__name__icontains=name)
+            elif choice_name == 'official_web':
+                games = games.filter(offical_web__name__icontains=name)
+            elif choice_name == 'platform':
+                games = games.filter(platform__name__icontains=name)
+     
+        if choice_price and price:
+            if choice_price == 'equal':
                 games = games.filter(price=price)
-            elif choice == 'less_than':
+            elif choice_price == 'less_than':
                 games = games.filter(price__lt=price)
-            elif choice == 'less_than_equal':
+            elif choice_price == 'less_than_equal':
                 games = games.filter(price__lte=price)
-            elif choice == 'greater_than':
+            elif choice_price == 'greater_than':
                 games = games.filter(price__gt=price)
-            elif choice == 'greater_than_equal':
-                games = games.filter(price__gte=price)        
+            elif choice_price == 'greater_than_equal':
+                games = games.filter(price__gte=price)             
     else:
         games = Game.objects.all()
     paginator = Paginator(games, 4)
