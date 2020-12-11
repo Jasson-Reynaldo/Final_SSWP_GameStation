4f699619c1bba0cef28000e82010e9cd86186dd0
Jasson Reynaldo <58303071+JassonReynaldo@users.noreply.github.com>
2020-12-11 18:17:16 +0700

    Minor updates on urls.py and base.html

--- GameStation/GameList/urls.py
+++ GameStation/GameList/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from GameList.controllers import game_controller, platform_controller, genre_controller, publisher_controller, developer_controller, official_web_controller
+from GameList.controllers import game_controller, platform_controller, genre_controller, publisher_controller, developer_controller, official_web_controller, home_controller
 
 urlpatterns = [
+    path('', home_controller.index, name='home_index'),
 	path('game', game_controller.index, name='game_index'),	
 	path('platform', platform_controller.index, name='platform_index'),
     path('genre', genre_controller.index, name='genre_index'),	
