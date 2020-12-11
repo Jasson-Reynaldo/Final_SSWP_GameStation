471a3097f73eecc3b614442cb664b12bdcfa2200
Francis-Dwiputra-A <58261826+Francis-Dwiputra-A@users.noreply.github.com>
2020-12-11 14:36:09 +0700

    Updating admin.py

--- GameStation/GameList/admin.py
+++ GameStation/GameList/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
+from GameList.models import game, genre, developer, platform, publisher, official_web
 
+admin.site.register(game.Game)
+admin.site.register(genre.Genre)
+admin.site.register(developer.Developer)
+admin.site.register(platform.Platform)
+admin.site.register(publisher.Publisher)
+admin.site.register(official_web.Official_web)
